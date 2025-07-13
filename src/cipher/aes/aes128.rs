be48967853a8b781e7cdd31f7f1277929d1b72b4
W0v0B <2081969239@qq.com>
2025-07-13 21:10:16 +0800

    Complete aes128 key expansion

--- src/cipher/aes/aes128.rs
+++ src/cipher/aes/aes128.rs
@@ -32,6 +32,49 @@ pub struct Aes128 {
     expanded_key: [[u8; 4]; 44]
 }
 
+impl Aes128 {
+    fn key_expansion(key: &self::Aes128Key) -> [[u8; 4]; 44] {
+        // create 44 word array
+        let mut w = [[0u8; 4]; 44];
+        let nk = 4; // For AES-128, Nk = 4 words
+        let mut rcon_index = 0;
+
+        // copy key into the first Nk words
+        w[0].copy_from_slice(&key.as_ref()[0..4]);
+        w[1].copy_from_slice(&key.as_ref()[4..8]);
+        w[2].copy_from_slice(&key.as_ref()[8..12]);
+        w[3].copy_from_slice(&key.as_ref()[12..16]);
+
+        // generate the rest of the words
+        for i in nk..w.len() {
+            let mut temp = u32::from_be_bytes(w[i - 1]);
+
+            // apply transformations if i is a multiple of Nk.
+            if i % nk == 0 {
+                // rotate left word: [a, b, c, d] -> [b, c, d, a]
+                temp = temp.rotate_left(8);
+
+                // replace the bytes of temp with S_BOX
+                let mut bytes = temp.to_be_bytes();
+                for byte in &mut bytes {
+                    *byte = S_BOX[*byte as usize];
+                }
+                temp = u32::from_be_bytes(bytes);
+
+                // XOR with the RCON
+                temp ^= RCON[rcon_index];
+                rcon_index += 1;
+            }
+
+            // final XOR to get the new word
+            let prev_word = u32::from_be_bytes(w[i - nk]);
+            w[i] = (prev_word ^ temp).to_be_bytes();
+        }
+
+        w
+    }
+}
+
 impl BlockCipher for Aes128 {
     const BLOCK_SIZE: usize = 16;
     const KEY_SIZE: usize = 16;
@@ -39,7 +82,7 @@ impl BlockCipher for Aes128 {
     type Key = Aes128Key;
 
     fn new(key: &Self::Key) -> Self {
-        unimplemented!()
+        Self { expanded_key: Self::key_expansion(key) }
     }
 
     fn encrypt_block(&self, block: &mut Self::Block) {
